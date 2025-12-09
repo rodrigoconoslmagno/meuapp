4c7f176aa3bc1aec8fd7be26fb6ac9da88decb4d
Rodrigo Consolmagno <rodrigoconsolmagno@hotmail.com>
2025-12-09 08:42:43 -0300

    Ajustes na injecao da chave para rodar no railway

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,6 +26,7 @@ if [ "$ACTIVE_PROFILE" = "prod" ]; then
     # Usamos as credenciais que o Railway injetou (PGUSER, PGPASSWORD)
     SPRING_DATASOURCE_USERNAME="${DB_USER}"
     SPRING_DATASOURCE_PASSWORD="${DB_PASS}"
+    JWT_SECRET_KEY="${JWT_SECRET_KEY}"
     
 else
     # -- Ambiente Local/Desenvolvimento --
@@ -34,7 +35,7 @@ else
     SPRING_DATASOURCE_USERNAME=$(cat /run/secrets/pg_user)
     SPRING_DATASOURCE_PASSWORD=$(cat /run/secrets/pg_password)
     DB_NAME=$(cat /run/secrets/pg_db)
-    export JWT_SECRET_KEY=$(cat /run/secrets/jwt_key)
+    JWT_SECRET_KEY=$(cat /run/secrets/jwt_key)
 fi
 
 # Exporta todas as variáveis para o ambiente do contêiner
@@ -43,6 +44,7 @@ export DB_PORT
 export SPRING_DATASOURCE_USERNAME
 export SPRING_DATASOURCE_PASSWORD
 export DB_NAME
+export JWT_SECRET_KEY
 export SPRING_JPA_HIBERNATE_DDL_AUTO="${SPRING_JPA_HIBERNATE_DDL_AUTO}"
 
 echo "Configurando a conexão com: jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}"
@@ -54,7 +56,8 @@ export CATALINA_OPTS="$CATALINA_OPTS \
     -Dspring.datasource.url=jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME} \
     -Dspring.datasource.username=${SPRING_DATASOURCE_USERNAME} \
     -Dspring.datasource.password=${SPRING_DATASOURCE_PASSWORD} \
-    -Dspring.jpa.hibernate.ddl-auto=${SPRING_JPA_HIBERNATE_DDL_AUTO}"
+    -Dspring.jpa.hibernate.ddl-auto=${SPRING_JPA_HIBERNATE_DDL_AUTO} \
+    -Djwt.secret-key=${JWT_SECRET_KEY}"
 
 echo "Injetando propriedades da JVM via CATALINA_OPTS..."
 
